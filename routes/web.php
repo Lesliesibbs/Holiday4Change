af85cc5a5a4570cdb2599de0ebe420500af001c2
Tonikay <tonikay.foster@gmail.com>
2018-08-29 01:39:50 +0100

    initial commit
    Co-Authored-By: null <Lesliesibbs@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes, Test
+| Web Routes, Test1
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loadsed by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
